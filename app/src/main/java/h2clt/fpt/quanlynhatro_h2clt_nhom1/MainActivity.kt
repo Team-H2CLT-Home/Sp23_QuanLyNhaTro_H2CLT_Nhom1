f36d7973e4e5dd742a3ca3a76b9c3f819a78daaa
long2 <longmhph29041@fpt.edu.vn>
2023-03-18 10:17:40 +0700

    Test1

---
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/MainActivity.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.quanlynhatro_h2clt_nhom1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package h2clt.fpt.quanlynhatro_h2clt_nhom1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
