f28b8c2392d1995dbdc520637d3480d0ee2d5c63
Cuong <cuongncph28957@fpt.edu.vn>
2023-03-20 14:23:02 +0700

    cuongncph28957_back_activity

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/activity/ActivityPhongDangThue.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/activity/ActivityPhongDangThue.kt
@@ -1,12 +1,35 @@
 package h2clt.fpt.quanlynhatro_h2clt_nhom1.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import h2clt.fpt.quanlynhatro_h2clt_nhom1.R
+import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.ActivityPhongDangThueBinding
+import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.ActivityPhongTrongBinding
 
 class ActivityPhongDangThue : AppCompatActivity() {
+    private lateinit var binding: ActivityPhongDangThueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_phong_dang_thue)
+        binding = ActivityPhongDangThueBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tbPhongDangThue
+        setSupportActionBar(binding.tbPhongDangThue)
+        val ab = getSupportActionBar()
+        ab?.setHomeAsUpIndicator(R.drawable.black_left)
+        ab?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun chuyenActivity(){
+        val intent = Intent(this@ActivityPhongDangThue, ActivityManHinhChinhChuTro::class.java)
+        startActivity(intent)
+        finish()
+    }
+    override fun  onOptionsItemSelected(item : MenuItem): Boolean {
+        val id : Int = item.getItemId();
+        if (id==android.R.id.home)
+            chuyenActivity();
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/activity/ActivityPhongTrong.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/activity/ActivityPhongTrong.kt
@@ -1,12 +1,35 @@
 package h2clt.fpt.quanlynhatro_h2clt_nhom1.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import h2clt.fpt.quanlynhatro_h2clt_nhom1.R
+import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.ActivityDanhSachPhongChuaTaoHoaDonBinding
+import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.ActivityPhongTrongBinding
 
 class ActivityPhongTrong : AppCompatActivity() {
+    private lateinit var binding: ActivityPhongTrongBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_phong_trong)
+        binding = ActivityPhongTrongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tbPhongTrong
+        setSupportActionBar(binding.tbPhongTrong)
+        val ab = getSupportActionBar()
+        ab?.setHomeAsUpIndicator(R.drawable.black_left)
+        ab?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun chuyenActivity(){
+        val intent = Intent(this@ActivityPhongTrong, ActivityManHinhChinhChuTro::class.java)
+        startActivity(intent)
+        finish()
+    }
+    override fun  onOptionsItemSelected(item : MenuItem): Boolean {
+        val id : Int = item.getItemId();
+        if (id==android.R.id.home)
+            chuyenActivity();
+        return super.onOptionsItemSelected(item);
     }
 }
