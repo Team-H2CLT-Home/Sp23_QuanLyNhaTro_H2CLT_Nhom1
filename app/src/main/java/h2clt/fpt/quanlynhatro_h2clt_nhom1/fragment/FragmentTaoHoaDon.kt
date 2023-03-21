388d15aed911031333c73abd3b23014818429872
root <nhh.tcp@gmail.com>
2023-03-21 09:53:52 +0700

    hiepnhph28873 sửa

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/fragment/FragmentTaoHoaDon.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/fragment/FragmentTaoHoaDon.kt
@@ -1,12 +1,8 @@
 package h2clt.fpt.quanlynhatro_h2clt_nhom1.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.R
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.activity.ActivityManHinhChinhChuTro
 import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.FragmentTaoHoaDonBinding
 
 
@@ -16,22 +12,8 @@ class FragmentTaoHoaDon : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = FragmentTaoHoaDonBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tbTaoHoaDon
-        setSupportActionBar(binding.tbTaoHoaDon)
-        val ab = getSupportActionBar()
-        ab?.setHomeAsUpIndicator(R.drawable.black_left)
-        ab?.setDisplayHomeAsUpEnabled(true)
+        binding.listViewTaoHoaDon.adapter
+
     }
 
-    fun chuyenActivity(){
-        val intent = Intent(this@FragmentTaoHoaDon, ActivityManHinhChinhChuTro::class.java)
-        startActivity(intent)
-        finish()
-    }
-    override fun  onOptionsItemSelected(item : MenuItem): Boolean {
-        val id : Int = item.getItemId();
-        if (id==android.R.id.home)
-            chuyenActivity();
-        return super.onOptionsItemSelected(item);
-    }
 }
