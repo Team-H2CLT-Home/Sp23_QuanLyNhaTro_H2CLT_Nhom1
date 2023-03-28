0d770be91c1dcedef6c820bf31d00ca5e05f11c5
long2 <longmhph29041@fpt.edu.vn>
2023-03-28 09:53:09 +0700

    dev

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/fragment/FragmentTaoHoaDon.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/fragment/FragmentTaoHoaDon.kt
@@ -1,36 +1,16 @@
 package h2clt.fpt.quanlynhatro_h2clt_nhom1.fragment
 
-import android.content.DialogInterface
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.R
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.activity.ActivityManHinhChinhChuTro
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.database.HoaDonDao
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.database.PhongDao
 import h2clt.fpt.quanlynhatro_h2clt_nhom1.databinding.FragmentTaoHoaDonBinding
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.model.HoaDon
-import h2clt.fpt.quanlynhatro_h2clt_nhom1.model.Phong
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class FragmentTaoHoaDon : AppCompatActivity() {
     private lateinit var binding: FragmentTaoHoaDonBinding
-    val list = mutableListOf<Phong>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentTaoHoaDonBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.listViewTaoHoaDon.adapter
-
 
     }
 
