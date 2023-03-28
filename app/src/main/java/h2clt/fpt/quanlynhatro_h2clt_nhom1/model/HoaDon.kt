75a4f711e3342e7dda9e21a2a0b4ee0b57a5ba4b
long2 <longmhph29041@fpt.edu.vn>
2023-03-28 09:39:16 +0700

    longmhph29041_hoaDon chưa dịch vụ

--- app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/model/HoaDon.kt
+++ app/src/main/java/h2clt/fpt/quanlynhatro_h2clt_nhom1/model/HoaDon.kt
@@ -4,12 +4,12 @@ data class HoaDon(
     val ma_hoa_don:String,
     val gia_thue:Int,
     val ngay_tao_hoa_don:String,
-    val trang_thai_hoa_don:Int,
+    var trang_thai_hoa_don:Int,
     val so_dien:Int,
     val so_nuoc:Int,
     val gia_dich_vu:Int,
     val mien_giam:Int,
-    val ma_phong:String){
+    var ma_phong:String){
     companion object{
         const val TB_NAME="hoa_don"
         const val CLM_MA_HOA_DON="ma_hoa_don"
